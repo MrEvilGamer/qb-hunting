cdf7d7fa2aa6f35cf57296f0506f139f8c76802e
Aditya Nath <84228628+MrEvilGamer@users.noreply.github.com>
2022-01-19 01:16:37 +0530

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -10,6 +10,8 @@ local Keys = {
 	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
 }
 
+QBCore = exports['qb-core']:GetCoreObject()
+
 CreateThread(function()
 	AddTextEntry("Hunting", "Hunting Spot")
 	for k, v in pairs(Config.Hunting) do
